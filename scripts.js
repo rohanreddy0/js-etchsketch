fd7159a90ae0d6ca929115f36c3fe96b26e339bc
Rohan <rohanreddy027@gmail.com>
2024-09-29 15:56:24 +0100

    Created initial page layout, styling and script

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let page_container = document.querySelector('#page-container');
+
+function createGridContainer() {
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
+
+}
+
