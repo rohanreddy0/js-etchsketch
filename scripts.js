66edcaab10cec050fa78d8c779dc9db059d2da39
Rohan <rohanreddy027@gmail.com>
2024-09-29 17:31:52 +0100

    Centered grid to page and enabled grid size functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-let grid_size = 16;
+// Grid Size
+let grid_size_picker = document.querySelector('#grid-size');
+let grid_size = parseInt(grid_size_picker.value);
+grid_size_picker.addEventListener('change', () => {
+    console.log(grid_size_picker.value);
+    grid_size = parseInt(grid_size_picker.value);
+    page_container.innerHTML = '';
+    rowPropagate();
+});
 
+// Create grids
 let page_container = document.querySelector('#page-container');
-
 function createGridRow() {
     const gridRowContainer = document.createElement('div');
     gridRowContainer.classList.add('grid-row-container');
@@ -13,7 +21,7 @@ function createGridRow() {
     page_container.appendChild(gridRowContainer);
 }
 
-function rowPropagate () {
+function rowPropagate() {
     for (let i = 1; i <= grid_size; i++) {
         createGridRow();
     }
@@ -21,15 +29,14 @@ function rowPropagate () {
 
 // Runtime Code
 rowPropagate();
-
 const squares = document.querySelectorAll('.grid-square');
 console.log(squares);
 
 for (let i = 0; i <= squares.length; i++) {
-    squares[i].addEventListener('mouseover', (event) => {
+    squares[i].addEventListener('mouseover', () => {
         squares[i].style.backgroundColor = 'red';
     });
-    squares[i].addEventListener('mouseout', (event) => {
+    squares[i].addEventListener('mouseout', () => {
         squares[i].style.backgroundColor = '#BBBBBB';
     })
 }
