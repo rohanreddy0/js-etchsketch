fee5375779e3b09978b39b9ea814debaec6118cc
Rohan <rohanreddy027@gmail.com>
2024-09-29 16:14:35 +0100

    Created js for row of squares

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 let page_container = document.querySelector('#page-container');
 
-function createGridContainer() {
-    const gridContainer = document.createElement('div');
-    gridContainer.classList.add('grid-container');
-
+function createGridRow() {
+    for (let i = 1; i <= 16; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        page_container.appendChild(gridRow);
+    }
 }
+
+createGridRow();
 
